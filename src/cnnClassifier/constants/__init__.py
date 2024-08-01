624f45e72d4ffcc77c292b8238f5f274aeaefff4
Kartik Nimhan <kartiknimhan9929@gmail.com>
2024-08-01 19:43:34 +0530

    Prepared data

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("D:\End to end machine learning projects\Kidney Disease classification predication\Kidney-Disease-Classification-Deep-Learning-Project\config\config.yaml")
+PARAMS_FILE_PATH = Path("D:\End to end machine learning projects\Kidney Disease classification predication\Kidney-Disease-Classification-Deep-Learning-Project\params.yaml")
+
+print(f"CONFIG_FILE_PATH: {CONFIG_FILE_PATH}")
+print(f"PARAMS_FILE_PATH: {PARAMS_FILE_PATH}")
